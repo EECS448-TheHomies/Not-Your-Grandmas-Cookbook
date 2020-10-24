1b576da6d0d5d0a31fdb9d927ba74d71d014a930
Thomas Atkins <dev.csnova@gmail.com>
2020-10-24 18:56:33 -0500

    GUI is working. Need to conect to the data structur

--- src/RecipeGui.py
+++ src/RecipeGui.py
@@ -29,6 +29,17 @@ class RecipeGui(object):
         # This prints the debug prints to a window
         sg.Print(do_not_reroute_stdout=False)
 
+
+    def collapse(self, layout, key):
+        """
+        Helper function that creates a Column that can be later made hidden, thus appearing "collapsed". Modified from pysimpleGUI cookbook
+        :param layout: The layout for the section
+        :param key: Key used to make this seciton visible / invisible
+        :return: A pinned column that can be placed directly into your layout
+        :rtype: sg.pin
+        """
+        return sg.pin(sg.Column(layout, key=key, visible=False))
+
     def run(self):
         """ Runs the GUI
 
@@ -40,7 +51,7 @@ class RecipeGui(object):
         longtext = """Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."""
         # Defines the summary framed element
         summary_layout = [
-            [sg.Text('Summary', font=self.sectionFont,enable_events=True)],
+            [sg.Text('Summary', font=self.sectionFont, enable_events=True)],
             [sg.T(textwrap.fill(longtext, self.width), font=self.bodyFont)],
             [sg.Text('_'*(self.width-18), font=self.bodyFont)]
         ]
@@ -64,25 +75,31 @@ class RecipeGui(object):
         ]
 
         # Defines the Ingredients framed element
-        list_col_instructions = [] # Holds the Instruction columns  
-        state_of_instructions = [] # If the text is hidden
+        list_col_instructions = []  # Holds the Instruction columns
+        state_of_instructions = []  # If the text is hidden
         for step in range(10):
-            name =" Step " + str(step) + ":"
+            name = " Step " + str(step) + ":"
             details = textwrap.fill(longtext, self.width)
 
+            elm_state = sg.Text(SYMBOL_UP, key='--Step:' +
+                                str(step)+':SYM--', enable_events=True)
+            elm_name = sg.Text(name, font=self.subSectionFont,
+                               enable_events=True, key='--Step:'+str(step)+':TITLE--')
+
+            elm_details = sg.Text(
+                details, font=self.bodyFont)
+
             # Appended the step to steps list
-            list_col_instructions.append([sg.Column([ [sg.Text(SYMBOL_UP, key='--Step:'+str(step)+':SYM--',enable_events=True), sg.Text(name, font=self.subSectionFont,enable_events=True,key='--Step:'+str(step)+':TITLE--')], [
-                sg.Text(details, font=self.bodyFont,visible=False, key='--Step:'+str(step)+':TEXT--')]])])
+            list_col_instructions.append([[elm_state, elm_name], [self.collapse([[elm_details]], '--Step:'+str(step)+':TEXT--') ]] )
 
             # set the default state to be hidden
             state_of_instructions.append(False)
-           
 
         instructions_layout = [
             [sg.Text('Instructions', font=self.sectionFont)]]
 
         for col in list_col_instructions:
-            instructions_layout += [col]
+            instructions_layout += col
             pass
 
         instructions_layout += [[sg.Text('_' *
@@ -114,10 +131,9 @@ class RecipeGui(object):
             elif event == 'New Timer':
                 print("Make a new Timer")
 
-
             for step in range(len(list_col_instructions)):
                 if event == '--Step:'+str(step)+':SYM--' or event == '--Step:'+str(step)+':TITLE--':
-                   
+
                     if state_of_instructions[step]:
                         state_of_instructions[step] = False
 
@@ -126,16 +142,12 @@ class RecipeGui(object):
                         window['--Step:'+str(step)+':TEXT--'](visible=False)
                         window.Refresh()
 
-
-
                     else:
-                        state_of_instructions[step] = True 
+                        state_of_instructions[step] = True
                         print("Expand Step " + str(step))
                         window['--Step:'+str(step)+':SYM--'](SYMBOL_DOWN)
                         window['--Step:'+str(step)+':TEXT--'](visible=True)
                         window.Refresh()
-
-
 
         window.close()
 
