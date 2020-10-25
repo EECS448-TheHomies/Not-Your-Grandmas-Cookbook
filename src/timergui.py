ddb5f23ae715e6d558b10a130b37fcc5e7d3414b
Edwin Recinos <e463r242@ku.edu>
2020-10-24 20:32:14 -0500

    buttons added and tested

--- src/timergui.py
+++ src/timergui.py
@@ -8,8 +8,9 @@ layout = [
             [  sg.Text( font=('Arial', 45), size=(10,1), key='OUTPUT1', justification = 'center') ],
  #         [   sg.Text('Food being cooked: '), sg.Output()],
  #           [  sg.Text('Type something in'), sg.Input()  ], 
-            [  sg.Button('Start'),  sg.Button('Pause'), sg.Button('Exit'), sg.Button('Add 15mins'), 
-            sg.Button('Subtract 15mins')]  ]                           
+            [  sg.Button('Start'),  sg.Button('Pause'), sg.Button('Exit')],
+            [ sg.Button('+15mins'), sg.Button('-15mins'),  sg.Button('+1min'), sg.Button('-1min'),  sg.Button('+30secs'), sg.Button('-30secs')] 
+               ]                           
 
 window = sg.Window("FOOD TIME!", layout)
 
@@ -31,17 +32,39 @@ while True:
         timr= False
 
 
-    elif event in ('Add 15mins'):
+    elif event in ('+15mins'):
         if timr==False:
             count=count+1500
             window['OUTPUT1'].update('{:02d}:{:02d}.{:02d}'.format((count // 100) // 60, (count // 100) % 60, count % 100))
             
     
-    elif event in ('Subtract 15mins'):
+    elif event in ('-15mins'):
         if timr == False:
             count=count-1500
             window['OUTPUT1'].update('{:02d}:{:02d}.{:02d}'.format((count // 100) // 60, (count // 100) % 60, count % 100))
             
+    elif event in ('+1min'):
+        if timr==False:
+            count=count+100
+            window['OUTPUT1'].update('{:02d}:{:02d}.{:02d}'.format((count // 100) // 60, (count // 100) % 60, count % 100))
+            
+    
+    elif event in ('-1min'):
+        if timr == False:
+            count=count-100
+            window['OUTPUT1'].update('{:02d}:{:02d}.{:02d}'.format((count // 100) // 60, (count // 100) % 60, count % 100))
+
+    elif event in ('+30secs'):
+        if timr==False:
+            count=count+30
+            window['OUTPUT1'].update('{:02d}:{:02d}.{:02d}'.format((count // 100) // 60, (count // 100) % 60, count % 100))
+            
+        
+    elif event in ('-30secs'):
+        if timr == False:
+            count=count-30
+            window['OUTPUT1'].update('{:02d}:{:02d}.{:02d}'.format((count // 100) // 60, (count // 100) % 60, count % 100))
+        
 
     if timr == True:
         window['OUTPUT1'].update('{:02d}:{:02d}.{:02d}'.format((count // 100) // 60, (count // 100) % 60, count % 100))
