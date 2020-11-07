bb66ce062f8d364d5fd149f66310f6b8b14a7926
cb549 <70673772+cb549@users.noreply.github.com>
2020-11-06 21:36:37 -0600

    Combined find_recipes & load_recipe and added loadAllRecipes

--- src/CookBook.py
+++ src/CookBook.py
@@ -2,9 +2,9 @@
 #handles interfacing
 #contains recipe objects
 
-import yaml
 import os
 import spoonacular as sp
+import yaml
 from .Recipe import Recipe
 class CookBook:
 
@@ -12,11 +12,11 @@ class CookBook:
 #Has Recipes
 #Loads recipes from files
     def __init__(self):
-        api = sp.API("79cd1cbf518f4039988fd991e9977bd8")
+        self.api = sp.API('79cd1cbf518f4039988fd991e9977bd8')
         self.recipeArr = []
-        self.parentDir = (os.getcwd()) + "\\"          # e.g. C:\\Users\Username\ProgramFiles\Project3Folder\
-        self.recipeDir = self.parentDir  +"Recipes\\"                 # e.g. C:\\Users\Username\ProgramFiles\Project3Folder\Recipes\
-
+        self.recipeDir = os.path.expanduser('~') + '\\Documents\\Recipes'          # e.g. C:\\Users\Username\Documents\Recipes
+        if not os.path.isdir(self.recipeDir):
+            os.makedirs(self.recipeDir)
 
     def loadFile(self,fileLoc):
     
@@ -24,56 +24,56 @@ class CookBook:
     
         with open(fileLoc) as file:
             yml = yaml.full_load(file)
-        recipeName = Recipe()
-        recipeName.init2(yml)
-        # i = 1
-        # while (1):                                                          # Here we are checking if there is a directory in existance, if not make one
-        #     if not os.path.isdir(self.recipeDir + yml["title"]):
-        #         os.makedirs(self.recipeDir + yml["title"])
-        #         recipeName.path = self.recipeDir + yml["title"]
-        #         break
-        #     elif not os.path.isdir(self.recipeDir + yml["title"] + "(" + str(i) + ")"):
-        #         os.makedirs(self.recipeDir + yml["title"] + "(" + str(i) + ")")
-        #         recipeName.path = self.recipeDir + yml["title"] + "(" + str(i) + ")"
-        #         break
-        #     i = i+1
-        # os.rename(fileLoc, self.recipeDir + "\\" + os.path.basename(recipeName.path) + "\\" + os.path.basename(fileLoc))
-        
-        # once the recipe has been created, we know there will be a directory for that recipe in \Recipes
-        # so we can move the .yaml file into that directory
-        
-        
+            recipeName = Recipe(yml)
+            file.close()
 
         self.recipeArr.append(recipeName)
         
+    def loadAllRecipes(self):
+        for filename in os.listdir(self.recipeDir):
+            try:
+                with open(self.recipeDir + '\\' + filename) as file:
+                    yml = yaml.full_load(file)
+                    self.recipeArr.append(Recipe(yml))
+                    file.close()
+            except:
+                os.remove(self.recipeDir + '\\' + filename)
+            
+        
     def find_recipes(self, query):
         """
-        Searches for a given query and returns an array with 10 entries, each entry contains a recipe related 
+        Searches for a given query and saves results to an array with 10 entries, each entry contains a recipe related 
         to the query with an ID, a title, a link to an image, and the image type located at index 0, 1, 2, and 3, respectively
+        Then it loops through the array and makes a call to the API for each index's ID.
         
         Arguments: 
             query (str): Query passed to the API to be searched for
         """
         response = self.api.search_recipes_complex(query)
-        data = response.json()["results"]
-        return data
-        
-    def save_recipe(self, list, index):
-        """
-        Saves the given recipe to a yaml file
-        
-        Arguments: 
-            data: A list of recipes that have an ID and title
-            index: The index of the desired recipe in the recipe list
-        """
-        id = str(list[index]["id"])
-        response = self.api._make_request("recipes/" + ID + "/information")
-        data = response.json()
-        with open(self.recipeDir + data["title"], 'w') as file:
-            doc = yaml.dump(data, file)
-        
-        
-        
+        results = response.json()['results']
+        for i in range(len(results)):
+            id = str(results[i]['id'])
+            path = 'recipes/' + id + '/information'
+            response = self.api._make_request(path)
+            res = response.json()
+            title = res['title'].replace('/','')
+            with open(self.recipeDir + '\\' + title + '.yml', 'w') as file:
+                doc = yaml.dump(res, file) 
+                file.close()
+                
+                
+                
+                
+cb = CookBook()
+cb.find_recipes('chicken parm')
+cb.loadAllRecipes()
+print('done')
 
-    def printRecipes(self, recipeName):
-        recipeName.printRecipe()
+
+
+
+
+
+
+
+
--- src/Recipe.py
+++ src/Recipe.py
@@ -9,28 +9,23 @@
 import os
 import spoonacular
 import yaml
+from bs4 import BeautifulSoup
 
 class Recipe:
     
     """Recipe stores recipe elements
-    Constructor count: 4
         def __init__(self): This sets variables within the class to be set to blank, when user wants to manually add ingredients
-        def init0(....): is used for project 4 where we will add the integration of an API
-        def init1(...): is used before the integration of the API, this will take 
-        def init2(...): is a constructor used in serialization
-
 
     Setters:
         Setter methods to set id, time, servings, url, image, summary, ingredients and the instructions
  
 
     """
-    def __init__(self):
-        """This constructor starts a blank Recipe object, all variables set to blank
-        These will be filled in by using the setter methods defined below the constructor, init methods
+    def __init__(self, yml):
+        """This constructor takes in a Yaml object, serialization
         
         Variables: 
-            path (str): str holding a path name
+            recipeDir (str): str holding a path name
             title (str): string that says title of recipe
             id (int): int holding id number of recipe
             time(int): int holding the time it would take to cook
@@ -46,88 +41,25 @@ class Recipe:
 
     # Constructor that creates an empty recipe
     
-        self.path = ""                                          # e.g. C:\\Users\Username\ProgramFiles\Project3Folder\recipes\"cake recipe"
-        self.title = ""
-        self.id = ""
-        self.time = ""
-        self.servings = ""
-        self.URL = ""
-        self.image = ""
-        self.summary = ""
+        self.recipeDir = os.path.expanduser('~') + '\\Documents\\Recipes'          # e.g. C:\\Users\Username\Documents\Recipes
+        self.title = yml['title']
+        self.id = yml['id']
+        self.time = yml['readyInMinutes']
+        self.servings = yml['servings']
+        self.URL = yml['sourceUrl']
+        self.image = yml['image']
+        soup = BeautifulSoup(yml['summary'], features='html.parser')                    # the BeautifulSoup library removes HTML tags from text
+        self.summary = soup.get_text()
         self.ingredients = []                                   # this will be a list of dictionaries that will contain details on the ingredients
-        self.instructions = []                                  # this will be a list of strings
+        soup = BeautifulSoup(yml['instructions'], features='html.parser')
+        instructions = soup.get_text('\n')
+        self.instructions = instructions.splitlines()                                  # this will be a list of strings
         
-    def init0(self, title, id, time, servings, URL, image, summary, ingredients, instructions):
-    
-        """This constructor is the post API integration, project 4 ready
-
-        Arguments: 
-            title (str): string that says title of recipe
-            id (int):  int holding id number of recipe
-            time(int): int holding the time it would take to cook
-            servings (int): int that says servings made
-            URL (str): string holding URL info
-            image (str): holds string to path showing image of food
-            summary (str): holds summary of food being made
-            ingredients (str array): array holding the ingredients used in food 
-            instructions (str array): array holding the instructions to make the food
-        
-        """
-    # initializer for when we know all elements of the recipe (post API)
-    
-        self.title = title
-        self.id = id
-        self.time = time
-        self.servings = servings
-        self.URL = URL
-        self.image = image
-        self.summary = summary
-        self.ingredients = ingredients
-        self.instructions = instructions
-
-    def init1(self, title, time, servings, summary, ingredients, instructions):
-        """This constructor takes in arguments from when object is made
-
-        Arguments: 
-            title (str): string that says title of recipe
-            time(int): int holding the time it would take to cook
-            servings (int): int that says servings made
-            summary (str): holds summary of food being made
-            ingredients (str array): array holding the ingredients used in food 
-            instructions (str array): array holding the instructions to make the food
-        
-        """
-    
-    # initializer for when we only know some elements of the recipe (pre API)
-        
-        self.title = title
-        self.time = time
-        self.servings = servings
-        self.summary = summary
-        self.ingredients = ingredients
-        self.instructions = instructions
-        
-    def init2(self, yml):        
-        """This constructor takes in a Yaml object, serialization
-
-        Constructor sets self.variables: 
-            title (str): string that says title of recipe
-            summary (str): holds summary of food being made
-            servings (int): int that says servings made
-            ingredients (str array): array holding the ingredients used in food 
-            instructions (str array): array holding the instructions to make the food
-        
-        """                                          
-    
-    # Initializer that takes a yaml object
-    # pass in the object that was read from the yaml file
-    
-        self.title = yml["title"]
-        self.summary = yml["summary"]
-        self.servings = yml["servings"]
-        self.ingredients = yml["ingredients"]                              # a dictionary with the indexes of "name", "amount", & "unit"
-        self.instructions = yml["instructions"]                            # a list of strings, each index is a separate step
-        i = 1
+        for i in range(len(yml['extendedIngredients'])):
+            name = yml['extendedIngredients'][i]['name']
+            amount = str(yml['extendedIngredients'][i]['measures']['us']['amount'])
+            unit = yml['extendedIngredients'][i]['measures']['us']['unitShort']
+            self.ingredients.append(name + ' | ' + amount + ' ' + unit)
         
     # Setter methods to assign values to member variables after construction
     
@@ -136,25 +68,18 @@ class Recipe:
         Argument: title (str)
         Action: title setter
         """
-        self.title = title
+        title = str(title)
         i = 1
         while (1):
-            if not os.path.isdir(self.recipeDir + title):
-                os.rename(r"" + self.path, r"" + self.recipeDir + title)
-                self.path = self.recipeDir + title
+            if not os.path.isdir(self.recipeDir + '\\' + title + '.yml'):
+                os.rename(self.recipeDir + '\\' + self.title, self.recipeDir + '\\' + title + '.yml')
+                self.recipeDir = self.recipeDir + + '\\' + title + '.yml'
                 break
-            elif not os.path.isdir(self.recipeDir + title + "(" + str(i) + ")"):
-                os.rename(r"" + self.path, r"" + self.recipeDir + title + "(" + str(i) + ")")
-                self.path = self.recipeDir + title + "(" + str(i) + ")"
+            elif not os.path.isdir(self.recipeDir + title + "(" + str(i) + ").yml"):
+                os.rename(self.recipeDir + '\\' + self.title, self.recipeDir + '\\' + title + "(" + str(i) + ").yml")
+                self.recipeDir = self.recipeDir + '\\' + title + "(" + str(i) + ").yml"
                 break
             i = i+1
-    
-    def setID(self, id):
-        """
-        Argument: id (int)
-        Action: function is a setter to set self.id = id (the argument passed in)
-        """
-        self.id = id
         
     def setTime(self, time):
         """
